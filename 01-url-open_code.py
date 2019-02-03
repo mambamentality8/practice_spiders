7dce120ad5691a8ed9fafc5fa9b3c3752aa66cca
侯世伟 <858968850@qq.com>
2019-02-03 16:25:28 +0800

    第一个小爬虫

---
+++ 01-url-open_code.py
@@ -0,0 +1,18 @@
+import urllib.request
+
+
+def load_data():
+    url = "http://www.baidu.com/"
+    # get请求
+    # http请求
+    # response:http相应的对象
+    response = urllib.request.urlopen(url)
+    # 读取内容
+    data = response.read()
+    str_data = data.decode("utf-8")
+    print(str_data)
+    #将数据写入文件
+    with open("baidu.html","w",encoding="utf-8") as f:
+        f.write(str_data)
+
+load_data()
